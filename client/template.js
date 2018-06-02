76f7c17257465db4666e0c01c0aa45a1d271d0e7
Omar De Leo <omar.deleo@gmail.com>
2018-06-01 22:40:22 -0400

    Add new font

--- client/template.js
+++ client/template.js
@@ -8,6 +8,7 @@ export default function() {
         <title>The Wall Street Journal & Breaking News, Business, Financial and Economic News, World News and Video</title>
         <link rel="shortcut icon" href="https://s.wsj.net/media/wsj_favicon-16x16.png" sizes="16x16" type="image/x-icon">
         <link rel="shortcut icon" href="https://s.wsj.net/media/wsj_favicon-32x32.png" sizes="32x32" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css?family=Prata" rel="stylesheet">
       </head>
       <body>
         <div id="root"></div>
