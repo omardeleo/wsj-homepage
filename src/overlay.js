965e98c21a1adc84f4bbc7f57bf0cba278b258cc
Omar De Leo <omar.deleo@gmail.com>
2018-06-02 20:31:40 -0400

    Add rating component

--- src/overlay.js
+++ src/overlay.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Rating from './rating.js';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 class Overlay extends Component {
@@ -14,33 +15,10 @@ class Overlay extends Component {
       {this.props.headline}
       <div className="summary">
         {date} - {this.props.summary}
-
-
       </div>
-      <div className="rating">
-        <div className="stars-yellow">
-          <FontAwesomeIcon icon={['fa', 'star']} />
-          <FontAwesomeIcon icon={['fa', 'star']} />
-          <FontAwesomeIcon icon={['fa', 'star']} />
-          <FontAwesomeIcon icon={['fa', 'star']} />
-          <FontAwesomeIcon icon={['fa', 'star']} />
-        </div>
-        <div className="stars-empty">
-          <FontAwesomeIcon icon={['far', 'star']} />
-          <FontAwesomeIcon icon={['far', 'star']} />
-          <FontAwesomeIcon icon={['far', 'star']} />
-          <FontAwesomeIcon icon={['far', 'star']} />
-          <FontAwesomeIcon icon={['far', 'star']} />
-        </div>
-      </div>
+      <Rating />
     </div>;
   }
 };
 
-//this is where you can define fallbacks for any props that don't get sent
-Overlay.defaultProps = {
-
-};
-
-//export this, or other files can't use this
 export default Overlay;
