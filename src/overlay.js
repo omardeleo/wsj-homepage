410b058301121c1f154bbb1d13bad42eab6b7786
Omar De Leo <omar.deleo@gmail.com>
2018-06-04 16:24:49 -0400

    Add views formatter

--- src/overlay.js
+++ src/overlay.js
@@ -7,10 +7,28 @@ class Overlay extends Component {
     super(props);
   }
 
+  viewsFormatter(views) {
+    if (views > 1000000) {
+      views = (views / 1000000).toFixed(2);
+      while (views.charAt(views.length - 1) === "0" ||
+        views.charAt(views.length - 1) === ".") {
+        views = views.substring(0, views.length -1);
+      }
+      return `${views}M`;
+    } else {
+      views = (views / 1000).toFixed(1);
+      views.charAt(views.length - 1) === "0" ? views = views.substring(0, views.length -2) : views;
+      return `${views}K`;
+    }
+    return views;
+  }
+
   render() {
     let date = new Date(this.props.date);
     const options = {month: "short", day: "2-digit"}
+    const {rating, views, mode} = this.props;
     date = date.toLocaleString('en-EN', options).toUpperCase();
+    let titleViews = `${views.toLocaleString('en-EN')} Views`;
     return <div className="overlay">
       <div>
         <div className="headline">
@@ -20,7 +38,10 @@ class Overlay extends Component {
           {date} - {this.props.summary}
         </div>
       </div>
-      <Rating rating={this.props.rating} mode={this.props.mode}/>
+      <div className="overlay-footer">
+        <Rating rating={rating} mode={mode}/>
+        <div className="views" title={titleViews}>{this.viewsFormatter(views)}</div>
+      </div>
     </div>;
   }
 };
