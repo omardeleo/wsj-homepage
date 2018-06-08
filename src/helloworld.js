9a9b19908816b6cc2015d4f35c1c02e7771af02d
Omar De Leo <omar.deleo@gmail.com>
2018-06-08 16:03:07 -0400

    Add ticker and quotes

--- src/helloworld.js
+++ src/helloworld.js
@@ -4,11 +4,24 @@ import React, { Component } from 'react';
 class HelloWorld extends Component {
 
   render() {
-    return <div className="header">
-              <div className="ticker"></div>
-            <h1>
-              <img src='assets/wsj-logo.svg' />
-            </h1>
+
+    return <div className="header-container">
+            <div className="ticker">
+              <div className="quotes">
+                <div>DJIA25294.07 0.21%▲</div>
+                <div>S&P 5002776.86 0.23%▲</div>
+                <div>Nasdaq7646.34 0.15%▲</div>
+                <div>U.S. 10 Yr-5/32 Yield2.939%▼</div>
+                <div>Crude Oil65.68 -0.41%▼</div>
+                <div>Euro1.1770 -0.25%▼</div>
+              </div>
+            </div>
+            <div className="header">
+
+              <h1>
+                <img src='assets/wsj-logo.svg' />
+              </h1>
+            </div>
           </div>;
   }
 };
